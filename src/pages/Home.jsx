075d52f4575fec7d809ed38913c87b56d5738580
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-11-29 21:58:39 +0100

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      
+        <div className="right">
+        <img className='logo' src="Wellness-Hub.png" alt="" />
+        </div>
     </div>
   )
 }
