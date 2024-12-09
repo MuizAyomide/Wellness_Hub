a8530b306aa130260b814488d2a3e91d6035f822
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-12-09 18:54:19 +0100

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-        <div className="right">
-        <img className='logo' src="Wellness-Hub.png" alt="" />
-        </div>
+      <div className="right">
+        <img className="logo" src="Wellness-Hub.png" alt="" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
